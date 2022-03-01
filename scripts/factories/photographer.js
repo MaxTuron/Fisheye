ce98fea970c37e1341f82288c73ebf504e53aec6
MaxTuron <@=@4d~Yx|bAiu|`Yv!$N>
2022-03-01 14:04:33 +0100

    1ere version de la page d'accueil.

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,44 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name,id,city,country,tagline,price,portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+
+        const lien = document.createElement( 'a' );
+        lien.setAttribute("href", 'photographers.html?id='+id)
+
+        const nom = document.createElement( 'h2' );
+        nom.textContent = name;
+
+        const ville = document.createElement( 'p' );
+        ville.textContent = city+",";
+        ville.classList.add("location");
+
+        const pays = document.createElement( 'p' );
+        pays.textContent = country;
+        pays.classList.add("location");
+
+        const description = document.createElement( 'p' );
+        description.textContent = tagline;
+
+        const prix = document.createElement( 'p' );
+        prix.classList.add("prix");
+        prix.textContent = price+"€/jour";
+
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(lien);
+        lien.appendChild(img);
+        article.appendChild(nom);
+        article.appendChild(ville);
+        article.appendChild(pays);
+        article.appendChild(description);
+        article.appendChild(prix);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name,city,country,tagline,price,picture, getUserCardDOM }
 }
