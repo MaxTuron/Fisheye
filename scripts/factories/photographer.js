53bcc8515602da172c2f3a3b3eadece247b2b1e8
MaxTuron <@=@4d~Yx|bAiu|`Yv!$N>
2022-03-14 10:55:54 +0100

    Optimisation factory photographer.

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,39 +5,18 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.innerHTML = `
+        <a href="photographer.html?id=${id}">
+            <img src="${picture}">
+        </a>
+        <h2> ${name}</h2>
+        <p class="location"> ${city} 
+            <span class="location">${country}</span> 
+        </p>
+        <p>${tagline}</p>
+        <p class="prix">${price}/jour </p>
+        `;
 
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-
-        const lien = document.createElement( 'a' );
-        lien.setAttribute("href", 'photographer.html?id='+id)
-
-        const nom = document.createElement( 'h2' );
-        nom.textContent = name;
-
-        const ville = document.createElement( 'p' );
-        ville.textContent = city+",";
-        ville.classList.add("location");
-
-        const pays = document.createElement( 'span' );
-        pays.textContent = " "+country;
-        pays.classList.add("location");
-
-        const description = document.createElement( 'p' );
-        description.textContent = tagline;
-
-        const prix = document.createElement( 'p' );
-        prix.classList.add("prix");
-        prix.textContent = price+"€/jour";
-
-        article.appendChild(img);
-        article.appendChild(lien);
-        lien.appendChild(img);
-        article.appendChild(nom);
-        article.appendChild(ville);
-        ville.appendChild(pays);
-        article.appendChild(description);
-        article.appendChild(prix);
         return (article);
     }
     return { name,city,country,tagline,price,picture, getUserCardDOM }
