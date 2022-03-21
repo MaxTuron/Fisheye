0adcfaab3a38e5ddbb3755b2667680384c57ef11
MaxTuron <@=@4d~Yx|bAiu|`Yv!$N>
2022-03-21 14:57:03 +0100

    Refactor CSS + test lightbox.

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,14 +1,13 @@
-function mediaFactory(data) {
-    const {title, id, likes, image, video} = data;
+function mediaFactory(data, numeroPhoto) {
+    let {title, id, likes, image, video} = data;
 
     function getMediaCardDOM() {
-        const picture = `assets/images/${image}`;
-        const coeur = `assets/icons/coeur.svg`;
-        const videos = `assets/video/${video}`;
-        const mediaID = data.id
-        const mediaLikes = data.likes
+        let picture = `assets/images/${image}`;
+        let coeur = `assets/icons/coeur.svg`;
+        let videos = `assets/video/${video}`;
+        let mediaID = data.id;
 
-        const article = document.createElement('article');
+        let article = document.createElement('article');
 
         if (image === undefined) {
             article.innerHTML = ` 
@@ -22,18 +21,20 @@ function mediaFactory(data) {
         `;
         } else {
             article.innerHTML = ` 
-             <a href="#${mediaID}">
-        <img class="thumb" alt="" src="${picture}">
-</a>
-
-<div class="lightbox" id="${mediaID}">
-    <a href="#_" class="btn-close">X</a>
-    <img src="${picture}" alt="">
-</div>
+             <img src="${picture}" alt="" onclick="openModal();currentSlide(${numeroPhoto})">
              <h2 class="titre_media">${title}
                 <button class="addLikes" data-id="${id}" onclick="addLike(${mediaID})">${likes}</button>
                  <img src="${coeur}" alt="" class="img_coeur">
-             </h2>      
+             </h2>  
+             
+             <div id="myModal" class="modal">
+                <span class="close cursor" onclick="closeModal()">&times;</span>
+                <div class="modal-content">
+                    <div class="mySlides">
+                        <img src="${picture}" style="width:100%">
+                    </div>    
+                </div>
+             </div>
         `;
         }
         return (article);
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,9 +4,9 @@ let IDphotographer = parseInt(searchParamsId);
 
 async function getPhotographers() {
     try {
-        const response = await fetch("./data/photographers.json");
-        const data = await response.json();
-        const photographers = await data.photographers;
+        let response = await fetch("./data/photographers.json");
+        let data = await response.json();
+        let photographers = await data.photographers;
         return {photographers}; //Retourne les données des photographes [Sous forme d'un tableau {d'objets}]
     } catch (error) {
         console.error(error);
@@ -15,9 +15,9 @@ async function getPhotographers() {
 
 async function getMedia() {
     try {
-        const response = await fetch("./data/photographers.json");
-        const data = await response.json();
-        const media = await data.media;
+        let response = await fetch("./data/photographers.json");
+        let data = await response.json();
+        let media = await data.media;
         return {media}; //Retourne les données des photographes [Sous forme d'un tableau {d'objets}]
     } catch (error) {
         console.error(error);
@@ -25,49 +25,55 @@ async function getMedia() {
 }
 
 
-async function displayData(photographers, media) {
-    const photographersSection = document.querySelector(".photographer_info");
-    const mediaSection = document.querySelector(".media_section");
-    const totalLikes = document.querySelector(".totalLikes");
-    const prix = document.querySelector(".prixPhoto");
+async function displayHeader(photographers) {
+    let photographersSection = document.querySelector(".photographer_info");
+    let prix = document.querySelector(".prixPhoto");
 
-    let compteurLikes = 0;
     let prixPhotographe = 0;
 
     photographersSection.innerHTML = `<button class="contact_button" onclick="displayModal()">Contactez-moi</button>`;
 
     photographers.forEach((photographer) => {
         if (photographer.id === IDphotographer) {
-            const photographerModel = photographerInfosFactory(photographer);
-            const photographerModelPhoto = photographerPhotoFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            const userCardDOMPhoto = photographerModelPhoto.getUserCardDOMPhoto();
+            let photographerModel = photographerInfosFactory(photographer);
+            let photographerModelPhoto = photographerPhotoFactory(photographer);
+            let userCardDOM = photographerModel.getUserCardDOM();
+            let userCardDOMPhoto = photographerModelPhoto.getUserCardDOMPhoto();
             photographersSection.appendChild(userCardDOM);
             photographersSection.appendChild(userCardDOMPhoto);
             prixPhotographe = photographer.price;
         }
     });
+    prix.innerHTML = prixPhotographe + "€/jour";
+};
+
+async function displayMedia(media) {
+    let mediaSection = document.querySelector(".media_section");
+    let totalLikes = document.querySelector(".totalLikes");
+
+    let compteurLikes = 0;
+    let numeroPhoto =0;
+
 
     media.forEach((medias) => {
         if (medias.photographerId === IDphotographer) {
-            const mediaModel = mediaFactory(medias);
-            const mediaCardDOM = mediaModel.getMediaCardDOM();
+            numeroPhoto = numeroPhoto +1;
+            let mediaModel = mediaFactory(medias, numeroPhoto);
+            let mediaCardDOM = mediaModel.getMediaCardDOM();
             mediaSection.appendChild(mediaCardDOM);
             compteurLikes = compteurLikes + medias.likes;
         }
     });
     console.log(compteurLikes)
     totalLikes.innerHTML = compteurLikes;
-    prix.innerHTML = prixPhotographe + "€/jour";
+
 };
 
 async function addLike(id) {
-    const {media} = await getMedia();
+    let {media} = await getMedia();
 
-    const mediaID = id;
-
-    const totalLikes = document.querySelector(".totalLikes");
-    const mediaLikes = document.querySelector("[data-id=" + CSS.escape(mediaID) + "]");
+    let totalLikes = document.querySelector(".totalLikes");
+    let mediaLikes = document.querySelector("[data-id=" + CSS.escape(id) + "]");
 
     let compteurTotal = totalLikes.innerHTML;
     let compteurMedia = mediaLikes.innerHTML;
@@ -87,9 +93,10 @@ async function addLike(id) {
 
 async function init() {
     // Récupère les datas des photographes
-    const {photographers} = await getPhotographers();
-    const {media} = await getMedia();
-    displayData(photographers, media);
+    let {photographers} = await getPhotographers();
+    let {media} = await getMedia();
+    displayHeader(photographers);
+    displayMedia(media);
 
 };
 
